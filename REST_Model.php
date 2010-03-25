613c0c8cbe2b7f0f9cc833611f08e0e8bad08aa8
alexedwards <ajmedwards@gmail.com>
2010-03-25 19:38:00 +0000

    neatening tabbing

--- REST_Model.php
+++ REST_Model.php
@@ -26,60 +26,60 @@ class REST_Model extends Model
 	);
 	
 		
-    function __construct()
-    {
-       	parent::Model();
-    }
-    
+	function __construct()
+	{
+		parent::Model();
+	}
+		
    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // REDIRECT THE REQUESTS TO THE $this->curl FUNCTION AS THEY COME IN
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// REDIRECT THE REQUESTS TO THE $this->curl FUNCTION AS THEY COME IN
    	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 	
 
 
-    function get ($url, $parameters = '')
-    {
-    	$this->curl("GET", $url, $parameters);
-    }
+	function get ($url, $parameters = '')
+	{
+		$this->curl("GET", $url, $parameters);
+	}
     
     
-    //---------------------------------------------------
+	//---------------------------------------------------
     
     
-    function post ($url, $parameters = '')
-    {
-    	$this->curl("POST", $url, $parameters);
-    }
-    
-    
-    //---------------------------------------------------
-    
-    
-    function delete ($url, $parameters = '')
-    {
-    	$this->curl("DELETE", $url, $parameters);
-    }
+	function post ($url, $parameters = '')
+	{
+		$this->curl("POST", $url, $parameters);
+	}
+		
+		
+	//---------------------------------------------------
+		
+		
+	function delete ($url, $parameters = '')
+	{
+	  	$this->curl("DELETE", $url, $parameters);
+	}
 
+		
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// SET CURL OPTIONS AND EXECUTE THE REQUEST
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 	
+		
     
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // SET CURL OPTIONS AND EXECUTE THE REQUEST
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 	
-    
-    
-    function curl ($method, $url, $parameters)
-    {	    	
+	function curl ($method, $url, $parameters)
+	{	    	
     
     	//---------------------------------------------------
     	// Initialise cURL handle and setup a few standard options
     	
-    	$ch = curl_init();
-    	curl_setopt($ch, CURLOPT_URL, $url);  
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);  
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array ("Accept: {$this->supported_formats[$this->format]}"));
 				
 				
 		//---------------------------------------------------
-    	// Set authentication options if needed
+		// Set authentication options if needed
 		
 		if ($this->authentication === 'basic')
    		{
@@ -94,15 +94,15 @@ class REST_Model extends Model
    		   		
    		   		
    		//---------------------------------------------------
-    	// Set parameters and any other remaining options
+		// Set parameters and any other remaining options
     	
-    	if (is_array($parameters))
-    	{
-    		$parameters = http_build_query($parameters);
-    	}
+		if (is_array($parameters))
+		{
+			$parameters = http_build_query($parameters);
+		}
     	
-    	switch ($method) 
-    	{
+		switch ($method) 
+		{
     		case 'GET':
     			if ($parameters !== '')
     			{
